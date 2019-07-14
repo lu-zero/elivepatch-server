8b9f4980987d9e8811d1efe5b95ce398533972a6
Luca Barbato <lu_zero@gentoo.org>
2019-07-14 11:03:37 +0000

    Do not depend on the cmdline_args
    Fixes #57

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import os.path
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 
-from elivepatch_server import create_app
+from elivepatch_server import app
 import pytest
 from flask_restful import Api as api
 from flask_testing import TestCase
@@ -11,5 +11,4 @@ from flask_testing import TestCase
 
 @pytest.fixture
 def app():
-    app = create_app("testing")
     return app
